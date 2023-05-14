022b6947a4bbd727357a55e41de5abb01be0db2e
20FacuSam23 <117329527+20FacuSam23@users.noreply.github.com>
2023-05-14 00:51:28 -0300

    Update MainSecurity.java

--- src/main/java/com/miaplicacion/primerproyecto/Security/MainSecurity.java
+++ src/main/java/com/miaplicacion/primerproyecto/Security/MainSecurity.java
@@ -42,7 +42,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().disable().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("**").permitAll()
+                .antMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
